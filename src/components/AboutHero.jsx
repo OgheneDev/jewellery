1fc103582d350629574f5a89eb67a2b61480a436
OgheneDev <emmanueloghene72@gmail.com>
2024-11-03 11:56:54 +0100

    background-video

--- src/components/AboutHero.jsx
+++ src/components/AboutHero.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 const AboutHero = () => {
   return (
-    <section className="bg-[url('/src/assets/images/bg.png')] bg-cover md:bg-[length:100%] bg-no-repeat w-full px-[30px] pt-[100px] py-[40px] md:pb-[100px] md:pt-[210px]">
-      <div className="card flex flex-col items-start gap-[20px] bg-card-bg px-[30px] py-[40px] md:w-[500px] md:ml-auto rounded-[10px]">
+    <section className="relative w-full px-[30px] pt-[100px] py-[40px] md:pb-[100px] md:pt-[210px]">
+      {/* Background video */}
+      <video
+        className="absolute top-0 left-0 w-full h-full object-cover"
+        src="https://res.cloudinary.com/dgc8cd67w/video/upload/v1730620223/VID-20241102-WA0039_cs3niu.mp4"
+        autoPlay
+        loop
+        muted
+      />
+      
+      {/* Overlay content */}
+      <div className="relative z-5 card flex flex-col items-start gap-[20px] bg-card-bg px-[30px] py-[40px] md:w-[500px] md:ml-auto rounded-[10px]">
         <article className='flex flex-col gap-[20px]'>
             <span className='uppercase text-dark-gold'>Burn and CO.</span>
             <h1 className='text-4xl text-gold'>Follow from Modelling to Production</h1>
@@ -12,7 +22,8 @@ const AboutHero = () => {
         <button className='text-white uppercase bg-gold py-[10px] px-[30px] md:py-[15px] md:px-[50px]'>Make an order</button>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default AboutHero
+export default AboutHero;
+
